bedb608bf465164c5a0f8138589eb03642b71c51
alexey.fansky <alexey.fansky@effective-soft.com>
2013-06-12 15:20:26 -0700

    removed names from resolutions enum

--- bugrap-domain/src/main/java/com/vaadin/training/bugrap/domain/entity/ReportResolution.java
+++ bugrap-domain/src/main/java/com/vaadin/training/bugrap/domain/entity/ReportResolution.java
@@ -1,19 +1,5 @@
 package com.vaadin.training.bugrap.domain.entity;
 
 public enum ReportResolution {
-    FIXED("Fixed"), INVALID("Invalid"), WONTFIX("Won't fix"), CANTFIX(
-            "Can't fix"), DUPLICATE("Duplicate"), WORKSFORME("Works for me"), NEEDMOREINFO(
-            "Need more Info");
-
-    private String name;
-
-    private ReportResolution(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
+    FIXED, INVALID, WONTFIX, CANTFIX, DUPLICATE, WORKSFORME, NEEDMOREINFO
 }
